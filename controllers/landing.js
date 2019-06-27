a61cec5fb153438899477f782efd22acf5af0ee0
megaMinded <megamindsetsolution@gmail.com>
2019-06-27 22:03:58 +0100

    Added bootstrap css using cdn

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,5 +1,12 @@
 exports.index = function (req, res, next) {
   res.render('landing', {
-    title: "This is a title"
+    title: "This is a title",
+    posts: [{
+      username: 'nzhope',
+      content: 'This is my post'
+    }]
   });
+}
+exports.submit_lead = function (req, res, next) {
+  console.log("Lead email:", req.body.email);
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 let landingPage = require('./../controllers/landing')
 /* GET home page. */
 router.get('/', landingPage.index);
+router.post('/', landingPage.submit_lead);
 
 
 module.exports = router;
